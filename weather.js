aa5bcfc051bdf0fbdc1694e4f57a1afceb18c86a
kjykim0 <73681150+kjykim0@users.noreply.github.com>
2025-02-16 00:54:04 +0900

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "75d681d1ed582a1efefae89b164f6502";
+const weather = document.querySelector("#weather span:first-child");
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+    });
+}
+
+function onGeoError(){
+    weather.innerText = "";
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+if(localStorage.getItem("username") !== null){
+    weather.innerText = "Required log in";
+} 
